465f789a6861ea483af30869ef7ffae89c4de5d3
xiangyx <xiangyx7@gmail.com>
2021-11-02 21:11:23 +0100

    ex1

---
+++ 365DS_Python_Bootcamp/EX1/3_1_exercise-print-function-task.py
@@ -0,0 +1,13 @@
+'''
+Print the following text using one 'print' function
+
+One,'two',"Three",four,\five\once,'I' caught a fish '//alive\\'
+'''
+print('One,')
+print('\'two\',')
+print('\"Three\",')
+print('four,')
+print('\\five\\once')
+print('\'I\' caught a fish \'//alive\\\\\'')
+
+print('One,\'two\',\"Three\",four,\n\\five\\once,\'I\' caught a\n\tfish \'//alive\\\\\'')
